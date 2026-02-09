66d046764e0887a88e797945151e72d31180f83b
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-02-09 20:17:04 +0530

    Added refreshToken end point

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { registerUser, loginUser, logoutUser } from '../controllers/user.controller.js';
+import { registerUser, loginUser, logoutUser, refreshAcessToken } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js'
 import { veriyJWT } from '../middlewares/auth.middleware.js';
 
@@ -20,6 +20,8 @@ router.post('/register', upload.fields([
 router.post('/login', loginUser);
 
 router.post('/logout', veriyJWT, logoutUser);
+
+router.post('/refresh-token', refreshAcessToken)
 
 
 
